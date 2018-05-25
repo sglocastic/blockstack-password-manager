6362fab336e4f7c6f2e0d0bac0a4426188f617ba
Stjepan Golemac <stjepan@locastic.com>
2018-05-25 15:29:50 +0200

    Serving port from env variable

--- serve.js
+++ serve.js
@@ -3,7 +3,9 @@ const cors = require('cors')
 const serveStatic = require('serve-static')
 
 const app = express()
+const PORT = process.env.PORT || 3000
 
 app.use(cors())
 app.use(serveStatic('dist', {'index': ['index.html' ]}))
-app.listen(3000)
+app.listen(PORT)
+console.log('server serving on port', PORT)
